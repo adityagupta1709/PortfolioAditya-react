884f3075f18d8ab48136aea52a670e6ccd4a19f9
Aditya Gupta <adityaguptafbg1709@gmail.com>
2022-06-19 20:03:37 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,121 @@
+const logotext = "ADITYA";
+const meta = {
+    title: "Aditya Gupta",
+    description: "Hey, I am Aditya Gupta  _ Full stack devloper",
+};
+
+const introdata = {
+    title: "I’m Aditya Gupta",
+    animated: {
+        first: "Full stack devloper",
+        second: "Designer",
+        third: "Tech enthusiast",
+    },
+    description: "MNIT Jaipur,Technical Team @ CACS (MNIT), Design Head at Entrepreneurship Development Cell, MNIT Jaipur",
+    your_img_url: "https://i.postimg.cc/RCWBkgQC/b612-tempphoto-edit-share-20220618-154651-307.jpg",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: " MNIT Jaipur,3rd year undergraduate BTech Electrical Engineering, Technical Team @ CACS (MNIT), Design Head at Entrepreneurship Development Cell, MNIT Jaipur , Mentee @TheUpliftProject'21, Designer Board at The Crescendo(Official Eco Business and Entrepreneurial news magazine of MNIT Jaipur ) ",
+};
+const ScholasticAchievements = [
+    {
+        jobtitle: "Among TOP 50 Teams",
+        where: "Hack36 5.0 MNNIT Allahabad Hackathon",
+        date: "2022",
+    },
+    {
+        jobtitle: "Designer of Month",
+        where: "The Crescendo (Official Eco Business and Entrepreneurial news magazine of MNIT Jaipur )",
+        date: "2022 Jan",
+    },
+];
+
+const skills = [{
+        name: "C/C++",
+        value: 90,
+    },
+    {
+        name: "HTML, CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "NodeJS, ExpressJS",
+        value: 65,
+    },
+];
+
+const services = [{
+        title: "Web Development",
+        description: "",
+    },
+    {
+        title: "UI & UX Design",
+        description: "",
+    },
+    {
+        title: "Android App Development",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://assets.devfolio.co/hackathons/d61a7c90d180427fa7ee90bce9ad0204/projects/ea14980cc0f14547b3caf94a867d4cfb/c4a4e934-4760-4bea-a9d7-2188a9b6b6a9.png",
+        desctiption: "theme Vocal for local which provide a platform for the local traders to get them self registered where they can put their own local product which could be later accessed by the ecommerce stores for selling",
+        link: "https://github.com/adityagupta1709/UrbanMerchant",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/81454329/174478086-6075e88b-5b8d-40aa-bec4-11dc0ec3180f.png",
+        desctiption: "Expense-tracker using Reactjs Progressive web application track your expenses",
+        link: "https://github.com/adityagupta1709/Expense-Tracker",
+    },
+    {
+        img: "https://user-images.githubusercontent.com/81454329/174482265-4932b8b8-47a8-4e51-8f86-490e84972c45.png",
+        desctiption: "● Built this project to test my knowledge on embedding images and videos in a webpage. ● Also wanted to see how well my understanding of CSS grid layouts is. ● Also applied javascript to apply my learning knowledge of that too.",
+        link: "https://github.com/adityagupta1709/Youtube-clone",
+    },
+      {
+        img: "https://assets.devfolio.co/hackathons/d61a7c90d180427fa7ee90bce9ad0204/projects/ea14980cc0f14547b3caf94a867d4cfb/bf826927-88b3-419d-85a5-cf0a87713d01.png",
+        desctiption: "Backend API to register and login users. API using which logged in user can add, edit and update their product.",
+        link: "#https://devfolio.co/projects/urbanmerchant-83ad",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "adityagupta1455@yahoo.com",
+    YOUR_FONE: "+918789616164",
+    description: "  ",
+    YOUR_SERVICE_ID: "service_rogkdlk",
+    YOUR_TEMPLATE_ID: "template_c9m6p1n",
+    YOUR_USER_ID: "aTUt70PxrDHPQ0yEM",
+};
+
+const socialprofils = {
+    github: "https://github.com/adityagupta1709",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/aditya-gupta-565b58202/",
+    twitter: "https://twitter.com/Adityagupta6790",
+    youtube: "https://www.youtube.com/channel/UCH96fbQc2FQibEAsQ0Ob88w",
+   
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    ScholasticAchievements,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
